abd0dff4d7d147e683c199ae95920cdc62f3763d
podsobnik93-collab <podsobnik93@gmail.com>
2026-07-25 20:51:30 +0300

    Add files via upload

---
+++ exercise-images.js
@@ -0,0 +1,7 @@
+// Реестр иллюстраций упражнений FitSim.
+// Ключ должен точно совпадать с названием упражнения в workout.js.
+const EXERCISE_IMAGES = {
+  'Отжимания широким хватом': 'images/exercises/push-ups.jpg',
+  'Отжимания с узкой постановкой рук': 'images/exercises/push-ups.jpg'
+};
+window.EXERCISE_IMAGES = EXERCISE_IMAGES;
